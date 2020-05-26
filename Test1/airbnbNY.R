f4ea6b8c3f9fd1729ec808efc12d13819bb91ba0
Andreaierardi <andreierardi@gmail.com>
2020-05-26 13:37:27 +0200

    up decision tree and RF

--- Test1/airbnbNY.R
+++ Test1/airbnbNY.R
@@ -5,8 +5,7 @@ library(tidyr)
 library(cowplot)
 library(magick)
 world_map <- map_data("newyork")
-dataset = read.csv("AB_NYC_2019.csv")
-theme_set(theme_cowplot())
+ds = read.csv("AB_NYC_2019.csv")
 
 # NOTES
 # Remove NA, empty
@@ -16,10 +15,16 @@ theme_set(theme_cowplot())
 #
 myplot = ggplot2::ggplot(dataset, aes(x = longitude, y = latitude, color= neighbourhood_group))+geom_point()
 
+clean_data = function(ds)
+{
+  ds$id = ds$host_id = ds$host_name = NULL
+  return(ds)
+}
 #ggdraw() +
 #  draw_image("New_York_City_.png") +
 #  draw_plot(myplot)
 
+dataset = clean_data(ds)
 mean(dataset$price )
 
 
@@ -50,7 +55,8 @@ averages
 
 library(randomForest)
 library(caTools)
-data_clean = select (dataset,-c(id,name,host_id,host_name, minimum_nights,number_of_reviews,neighbourhood,last_review,availability_365,reviews_per_month,,calculated_host_listings_count))
+library(dplyr)
+data_clean =select (dataset,-c(host_id, id, host_name, name,minimum_nights,number_of_reviews,neighbourhood,last_review,availability_365,reviews_per_month,calculated_host_listings_count))
 
 sample = sample.split(data_clean, SplitRatio = .75)
 train = subset(data_clean, sample == TRUE)
@@ -60,19 +66,46 @@ dim(train)
 dim(test)
 dim(data_clean)
 rf <- randomForest(
-  price ~ .,
-  data=train
+  neighbourhood_group ~ . ,
+  data=train,
+ # importance = TRUE
 )
 rf
 
 pred = predict(rf, newdata=test,na.action = na.pass)
 
+table(pred,test$neighbourhood_group)
+
+#https://www.r-bloggers.com/how-to-implement-random-forests-in-r/
+varImpPlot(rf)
+
+plot(pred)
 #cm = table(test[,14], pred)
 
-library(caret)
-confusionMatrix(as.factor(pred), as.factor(test$price))
+#library(caret)
+#confusionMatrix(as.factor(pred), as.factor(test$price))
 
-confusionMatrix(
-  factor(pred, levels = 1:19558),
-  factor(test$price, levels = 1:19558)
-)
+#confusionMatrix(
+#  factor(pred, levels = 1:19558),
+#  factor(test$price, levels = 1:19558)
+#)
+library(ROCR)
+#ROCR
+roc_pred = predict(rf,type="prob")
+perf = prediction(roc_pred[,2], train$neighbourhood_group)
+
+#==
+#FIND OPTIMAL VALUE WITH MIN OUT OF BAG
+#https://www.listendata.com/2014/11/random-forest-with-r.html
+
+
+#================
+# DECISION  TREE
+library(rpart)
+library(caret)
+library(e1071)
+
+
+dt = train( neighbourhood_group~ ., data = train,method = "rpart")
+pred2 = predict(dt, data = test)
+table(pred2, test)
