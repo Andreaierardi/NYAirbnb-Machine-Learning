e272bce2dc809a70c8af865b4fcc52ded8f9b178
Andreaierardi <andreierardi@gmail.com>
2020-06-02 18:51:41 +0200

    update random forest test

--- Test1/airbnbNY.R
+++ Test1/airbnbNY.R
@@ -4,8 +4,11 @@ library(ggmap)
 library(tidyr)
 library(cowplot)
 library(magick)
-world_map <- map_data("newyork")
+#world_map <- map_data("newyork")
 ds = read.csv("AB_NYC_2019.csv")
+
+#Check for NA
+apply(ds,2,function(x) sum(is.na(x)))
 
 # NOTES
 # Remove NA, empty
@@ -86,6 +89,160 @@ table(pred,test$neighbourhood_group)
 varImpPlot(rf)
 
 plot(pred)
+
+
+
+rf2 <- randomForest(
+  price ~ . ,
+  data=train,
+  # importance = TRUE
+)
+rf2
+varImpPlot(rf2)
+
+library(randomForest)
+library(caret)
+library(e1071)
+
+trControl <- trainControl(method = "cv",
+                          number = 10,
+                          search = "grid")
+
+rf_default <- train(
+  neighbourhood_group ~ . ,
+  data=train,
+  method = "rf",
+  metric="Accuracy",
+  trControl = trControl
+    # importance = TRUE
+)
+print(rf_default)
+
+
+set.seed(1234)
+tuneGrid <- expand.grid(.mtry = c(1: 10))
+rf_mtry <- train(
+                  neighbourhood_group~.,
+                 data = train,
+                 method = "rf",
+                 metric = "Accuracy",
+                 tuneGrid = tuneGrid,
+                 trControl = trControl,
+                 importance = TRUE,
+                 nodesize = 14,
+                 ntree = 300)
+print(rf_mtry)
+
+
+best_mtry <- rf_mtry$bestTune$mtry 
+best_mtry
+#[1] 4
+
+max(rf_mtry$results$Accuracy)
+#[1] 0.9987047
+
+
+store_maxnode <- list()
+tuneGrid <- expand.grid(.mtry = best_mtry)
+for (maxnodes in c(5: 15)) {
+  set.seed(1234)
+  rf_maxnode <- train(neighbourhood_group~.,
+                      data = train,
+                      method = "rf",
+                      metric = "Accuracy",
+                      tuneGrid = tuneGrid,
+                      trControl = trControl,
+                      importance = TRUE,
+                      nodesize = 14,
+                      maxnodes = maxnodes,
+                      ntree = 300)
+  current_iteration <- toString(maxnodes)
+  store_maxnode[[current_iteration]] <- rf_maxnode
+}
+results_mtry <- resamples(store_maxnode)
+summary(results_mtry)
+
+
+
+for (maxnodes in c(15 : 30)) {
+  set.seed(1234)
+  rf_maxnode <- train(neighbourhood_group~.,
+                      data = train,
+                      method = "rf",
+                      metric = "Accuracy",
+                      tuneGrid = tuneGrid,
+                      trControl = trControl,
+                      importance = TRUE,
+                      nodesize = 14,
+                      maxnodes = maxnodes,
+                      ntree = 300)
+  current_iteration <- toString(maxnodes)
+  store_maxnode[[current_iteration]] <- rf_maxnode
+}
+results_mtry <- resamples(store_maxnode)
+summary(results_mtry)
+
+#30
+
+
+#Step 4) Search the best ntrees
+#Now that you have the best value of mtry and maxnode, 
+#you can tune the number of trees. The method is exactly the same as maxnode.
+
+store_maxtrees <- list()
+for (ntree in c(250, 300, 350, 400, 450, 500, 550, 600, 800, 1000, 2000)) {
+  set.seed(5678)
+  rf_maxtrees <- train(neighbourhood_group~.,
+                       data = train,
+                       method = "rf",
+                       metric = "Accuracy",
+                       tuneGrid = tuneGrid,
+                       trControl = trControl,
+                       importance = TRUE,
+                       nodesize = 14,
+                       maxnodes = 24,
+                       ntree = ntree)
+  key <- toString(ntree)
+  store_maxtrees[[key]] <- rf_maxtrees
+}
+results_tree <- resamples(store_maxtrees)
+summary(results_tree)
+
+
+
+
+
+#Step 5) Evaluate the model
+fit_rf <- train(neighbourhood_group~.,
+                train,
+                method = "rf",
+                metric = "Accuracy",
+                tuneGrid = tuneGrid,
+                trControl = trControl,
+                importance = TRUE,
+                nodesize = 14,
+                ntree = 2000,
+                maxnodes = 30)
+
+
+
+
+prediction <-predict(fit_rf, test)
+
+prediction_default <-predict(rf, test)
+
+conf = confusionMatrix(prediction, test$neighbourhood_group)
+
+conf_default = confusionMatrix(prediction_default, test$neighbourhood_group)
+
+conf
+
+conf_default
+#Step 6) Visualize Result
+varImpPlot(fit_rf)
+
+
+
 #cm = table(test[,14], pred)
 
 #library(caret)
@@ -105,6 +262,10 @@ perf = prediction(roc_pred[,2], train$neighbourhood_group)
 #https://www.listendata.com/2014/11/random-forest-with-r.html
 
 
+
+
+
+
 #================
 # DECISION  TREE
 library(rpart)
@@ -115,3 +276,15 @@ library(e1071)
 dt = train( neighbourhood_group~ ., data = train,method = "rpart")
 pred2 = predict(dt, data = test)
 table(pred2, test)
+
+
+
+
+
+
+
+
+#============================
+## NEURAL NETWORKS
+
+# https://medium.com/@brscntyz/neural-network-in-r-e275302b6e44
