8a48e413ce8ac09e8972b400a9a9f7c5e3e2d9f8
Andreaierardi <andreierardi@gmail.com>
2020-06-07 13:57:11 +0200

    ranger and NN

--- Test1/airbnbNY.R
+++ Test1/airbnbNY.R
@@ -17,7 +17,6 @@ apply(ds,2,function(x) sum(is.na(x)))
 #
 #
 #
-myplot = ggplot2::ggplot(dataset, aes(x = longitude, y = latitude, color= neighbourhood_group))+geom_point()
 
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -43,8 +42,14 @@ clean_data = function(ds)
 #  draw_plot(myplot)
 
 dataset = clean_data(ds)
-mean(dataset$price )
 
+
+
+# ================== DATA VISUALISATION ==================
+
+#myplot = ggplot2::ggplot(dataset, aes(x = longitude, y = latitude, color= neighbourhood_group))+geom_point()
+
+mean(dataset$price )
 
 man = dataset  %>% 
   filter( neighbourhood_group== "Manhattan")
@@ -66,18 +71,12 @@ averages =  dataset %>%
     avg_price = mean(price, na.rm = TRUE)
   )
 averages
+# =========================================================
 
 
-#RANDOM FOREST
-#https://www.guru99.com/r-random-forest-tutorial.html
+# ================== TRAIN AND TEST SETS ==================
 
 
-# GOOD VIDEO FOR PARAMETER OPTIMISATION
-# https://www.youtube.com/watch?v=6EXPYzbfLCE
-
-# TUNING 
-# https://uc-r.github.io/random_forests
-library(randomForest)
 library(caTools)
 library(caret)
 
@@ -89,6 +88,23 @@ test  = subset(data_clean, sample == FALSE)
 dim(train)
 dim(test)
 dim(data_clean)
+
+# =========================================================
+
+
+
+# ================== RANDOM FOREST ==================
+
+#https://www.guru99.com/r-random-forest-tutorial.html
+
+
+# GOOD VIDEO FOR PARAMETER OPTIMISATION
+# https://www.youtube.com/watch?v=6EXPYzbfLCE
+
+# TUNING 
+# https://uc-r.github.io/random_forests
+library(randomForest)
+
 rf <- randomForest(
   neighbourhood_group ~ . ,
   data=train,
@@ -257,6 +273,63 @@ conf_default
 varImpPlot(fit_rf)
 
 
+# =========================================================
+
+# ================== RANGER RANDOM FOREST =================
+library(ranger)
+library(tuneRanger)
+ranger <- ranger( neighbourhood_group~ ., data = train, write.forest = TRUE, classification = T)
+
+ranger_pred = predict(ranger, data = test)
+
+tab = table(test$neighbourhood_group, predictions(ranger_pred))
+
+plot(tab)
+
+
+
+
+rangerReg <- ranger( price~ ., data = train, write.forest = TRUE, classification = F)
+rangerReg
+rangerReg_pred = predict(rangerReg, data = test)
+rangerReg_pred
+tab = table(test$price, predictions(rangerReg_pred))
+tab
+plot_ly(x = test$price, y = predictions(rangerReg_pred))
+
+
+
+# https://github.com/PhilippPro/tuneRanger
+# https://mlr.mlr-org.com/articles/tutorial/measures.html
+task = makeRegrTask(data = train, target = "price")
+estimateTimeTuneRanger(task, iters = 70, num.threads = 6, num.trees = 1000)
+res = tuneRanger(task, measure = list(mse), num.trees = 1000, 
+                 num.threads = 4, iters = 70,  show.info = getOption("mlrMBO.show.info", TRUE))
+
+
+# Mean of best 5 % of the results
+res
+#Recommended parameter settings: 
+#  mtry min.node.size sample.fraction
+#1    2            60       0.2010013
+#Results: 
+#  mse exec.time
+#1 0.0004812156     3.446
+
+# Model with the new tuned hyperparameters
+res$model
+#Model for learner.id=regr.ranger; learner.class=regr.ranger
+#Trained on: task.id = train; obs = 29337; features = 4
+#Hyperparameters: num.threads=4,verbose=FALSE,respect.unordered.factors=order,mtry=2,
+#min.node.size=60,sample.fraction=0.201,num.trees=1e+03,replace=FALSE
+
+tuned_rangerReg <- ranger( price~ ., data = train, write.forest = TRUE, classification = F, mtry= 2, 
+                           min.node.size = 60, sample.fraction = 0.201,num.trees = 1000, replace= FALSE)
+tuned_rangerReg
+tuned_rangerReg_pred = predict(tuned_rangerReg, data = test)
+tuned_rangerReg_pred
+
+# ==================== ROCK CURVE =========================
 
 #cm = table(test[,14], pred)
 
@@ -278,11 +351,14 @@ perf = prediction(roc_pred[,2], train$neighbourhood_group)
 
 
 
+# =========================================================
 
 
 
-#================
-# DECISION  TREE
+
+# ===================== DECISION  TREE ====================
+
+# 
 library(rpart)
 library(caret)
 library(e1071)
@@ -299,7 +375,74 @@ table(pred2, test)
 
 
 
-#============================
-## NEURAL NETWORKS
+# =========================================================
+
+
+
+# =================== NEURAL NETWORKS ??=====================
+
 
 # https://medium.com/@brscntyz/neural-network-in-r-e275302b6e44
+# https://datascienceplus.com/fitting-neural-network-in-r/
+# https://www.kdnuggets.com/2016/08/begineers-guide-neural-networks-r.html/2
+library(ISLR)
+library(tidyverse)
+library("keras")
+library(neuralnet)
+library(Hmisc)
+
+m <- model.matrix( 
+  ~price+neighbourhood_group+room_type+longitude+latitude,
+  data = train 
+)
+
+m_test  <- model.matrix( 
+  ~price+neighbourhood_group+room_type+longitude+latitude,
+  data = test 
+)
+
+head(m)
+
+nn=neuralnet(price~ neighbourhood_group2+ neighbourhood_group3 +neighbourhood_group4+ neighbourhood_group5+ room_type2+ room_type3+longitude+latitude,data=m, hidden=10,act.fct = "logistic",
+             linear.output = TRUE,stepmax=10^5,threshold = 0.01)
+
+
+#Also you can change your hidden layers by specifiying with numbers in vector like this
+nn=neuralnet( price~ neighbourhood_group2+ neighbourhood_group3 +neighbourhood_group4+ neighbourhood_group5+ room_type2+ room_type3+longitude+latitude,data=m, hidden=c(7,6,5),act.fct = "logistic",
+             linear.output = TRUE,stepmax=10^5,threshold = 0.01)
+
+hidden=c(7,6,5)
+
+
+#Then, prediction and calculation of error comes. I calculate the error with Root mean error method.
+nn_pred=compute(nn,test[,1:13])
+nn_pred$net.resultRMSE <- function(actual,predicted) {
+  return(sqrt(sum(actual^2-predicted^2)/length(actual)))
+}
+summary(nn_pred)
+nn_pred <- is.numeric(nn_pred)
+RMSE(test$price,nn_pred)
+
+plot(test$price,nn_pred$net.result)
+
+pr.nn_ <- nn_pred$net.result*(max(dataset$price)-min(dataset$price))+min(dataset$price)
+# =========================================================
+
+
+# ================= LINEAR REGRESSION =====================
+# https://datascienceplus.com/fitting-neural-network-in-r/
+lm.fit <- glm(price~., data=train)
+summary(lm.fit)
+pr.lm <- predict(lm.fit,test)
+MSE.lm <- sum((pr.lm - test$price)^2)/nrow(test)
+
+
+
+par(mfrow=c(1,2))
+plot(test$price,nn_pred$net.result,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='NN',pch=18,col='red', bty='n')
+plot(test$price,pr.lm,col='blue',main='Real vs predicted lm',pch=18, cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
+
